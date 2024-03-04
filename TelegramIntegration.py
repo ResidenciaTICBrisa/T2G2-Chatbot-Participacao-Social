4ca40bb4ff886dc9e5dc529ec6ab29c0b6863431
Isaque <isak369@hotmail.com>
2024-03-04 13:29:25 +0300

    Adicionando comentários

--- TelegramIntegration.py
+++ TelegramIntegration.py
@@ -3,29 +3,35 @@ import requests
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv()   # verifica se existe um arquivo .env
 
-token = os.getenv('TELEGRAM_TOKEN')
-bot = telebot.TeleBot(token=token)
+token = os.getenv('TELEGRAM_TOKEN')  # pega o valor do token do dotenv
+bot = telebot.TeleBot(token=token)  # inicia o bot determinado pelo token
 
 
-def get_answer_rasa(message) -> str:
-    headers = {'Content-Type': 'application/json'}
+def get_answer_rasa(message) -> list[dict]:
+    # essa função envia uma requisição POST para a url do rasa e recebe a resposta do RASA, retornando-a em um json
+    headers = {'Content-Type': 'application/json'}  # define o tipo de arquivo a ser enviado na requisição
     response = requests.post('http://localhost:5005/webhooks/rest/webhook',
                              json=
                              {"sender": "Rasa", "message": message.text},
                              headers=headers)
-    json_resp = response.json()
-    print(json_resp)
+    # envia a requisição
+    json_resp = response.json()  # recebe a requisição, converte pra json e armazena em uma variável
+    print(json_resp)    # nota-se que é uma lista de json, porque o chatbot pode responder multiplas vezes para /
+    # unica questão
     print(f"Resposta do RASA: {json_resp[0]['text']}")
-    return json_resp    #json
+    return json_resp  # retorna a lista de json
 
 
 @bot.message_handler(content_types=['text'])
+# decorator necessário para que a função receive_message possa receber as mensagens do telegram
 def receive_message(message):
+    # essa função fica rodando até que o script seja encerrado, fazendo pooling.
+    # portanto, não precisa ser chamada
     print(f'Pergunta: {message.text}')
-    answer = get_answer_rasa(message)
-    if len(answer) > 1:
+    answer = get_answer_rasa(message)   # envia a resposta para o rasa
+    if len(answer) > 1:  # if específico para o modelo padrão do rasa, modificar depois
         bot.send_message(chat_id=message.chat.id, text=answer[0]['text'])
         bot.send_photo(chat_id=message.chat.id, photo=answer[1]['image'])
         bot.send_message(chat_id=message.chat.id, text=answer[2]['text'])
