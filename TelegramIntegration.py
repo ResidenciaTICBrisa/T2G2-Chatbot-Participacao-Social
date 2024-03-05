82d11883d9527a124c47959d165b06a40d637d82
Gabriel Zaranza <116514986+GZaranza@users.noreply.github.com>
2024-03-05 14:13:26 -0300

    mudando o if/else para um loop

--- TelegramIntegration.py
+++ TelegramIntegration.py
@@ -18,7 +18,7 @@ def get_answer_rasa(message) -> list[dict]:
                              headers=headers)
     # envia a requisição
     json_resp = response.json()  # recebe a requisição, converte pra json e armazena em uma variável
-    print(json_resp)    # nota-se que é uma lista de json, porque o chatbot pode responder multiplas vezes para /
+    # print(json_resp)    # nota-se que é uma lista de json, porque o chatbot pode responder multiplas vezes para /
     # unica questão
     print(f"Resposta do RASA: {json_resp[0]['text']}")
     return json_resp  # retorna a lista de json
@@ -31,12 +31,17 @@ def receive_message(message):
     # portanto, não precisa ser chamada
     print(f'Pergunta: {message.text}')
     answer = get_answer_rasa(message)   # envia a resposta para o rasa
-    if len(answer) > 1:  # if específico para o modelo padrão do rasa, modificar depois
-        bot.send_message(chat_id=message.chat.id, text=answer[0]['text'])
-        bot.send_photo(chat_id=message.chat.id, photo=answer[1]['image'])
-        bot.send_message(chat_id=message.chat.id, text=answer[2]['text'])
-    else:
-        bot.send_message(chat_id=message.chat.id, text=answer[0]['text'])
-
-
+    
+    for item in answer:
+        #print(f'TESTEEE: {item}')
+        
+        try:
+            if item['text']:
+                bot.send_message(chat_id=message.chat.id, text=item['text'])
+            
+        except KeyError:
+            bot.send_photo(chat_id=message.chat.id, photo=item['image'])
+            
+    
 bot.polling(True)  # faz com que o código fique em loop
+
