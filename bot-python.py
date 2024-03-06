61dafdf7aac2438a3ed8f24d4debb72d6044ccb1
Gabriel <gabrielzaranza@hotmail.com>
2024-03-06 15:16:44 -0300

    criando o fluxo 7 do chatbot

--- bot-python.py
+++ bot-python.py
@@ -1,36 +1,72 @@
-user = input("Qual seu nome?")
-print("Bem-vindo(a) ao atendimento virtual do Brasil Participativo." +
+
+
+print("\nBem-vindo(a) ao atendimento virtual do Brasil Participativo." +
        "Para começar, diga-nos o seu nome")
+user = input("Qual seu nome? ")
 print(f'Olá, {user}. Digite o número que corresponde à opção que deseja acessar. \
-      \nCaso queira encerrar o atendimento, digite "sair" a qualquer momento')
+      \nCaso queira encerrar o atendimento, digite \"sair\" a qualquer momento')
 
 
-print("Digite 1 - O que é o Brasil Participativo?\n"+
-      "Digite 2 - Processos Participativos\n"+
-      "Digite 3 - Dificuldade de acesso\n"+
-      "Digite 4 - Represento um órgão da Administração Pública Federal\n"+
-      "Digite 5 - Sou da imprensa\n"+
-      "Digite 6 - Perguntas Frequentes\n"+
-      "Digite 7 - Perguntas Frequentes\n"
-      )
-opcao = int(input("Digite sua opção"))
+while(True):
+    print("Digite 1 - O que é o Brasil Participativo?\n"+
+        "Digite 2 - Processos Participativos\n"+
+        "Digite 3 - Dificuldade de acesso\n"+
+        "Digite 4 - Represento um órgão da Administração Pública Federal\n"+
+        "Digite 5 - Sou da imprensa\n"+
+        "Digite 6 - Perguntas Frequentes\n"+
+        "Digite 7 - Perguntas Frequentes\n"
+        )
+    opcao = input("Digite sua opção: ")
 
-match opcao:
-    case 1:
-        print("Você escolheu a opção 1 - O que é o Brasil Participativo?")
+
+
+
+    match opcao:
+        case "1":
+            print("Você escolheu a opção 1 - O que é o Brasil Participativo?")
+            
+        case "2":
+            print("Você escolheu a opção 2 - Processos Participativos")
+
+        case "3":
+            print("Você escolheu a opção 3 - Dificuldade de acesso")
+
+        case "4":
+            print("Você escolheu a opção 4 - Represento um órgão da Administração Pública Federal")
+        case "5":
+            print("Você escolheu a opção 5 - Sou da imprensa")
+        case "6":
+            print("Você escolheu a opção 6 - Perguntas Frequentes")
         
-    case 2:
-        print("Você escolheu a opção 2 - Processos Participativos")
+        case "7":
+            print("Você escolheu a opção 7 - Outros assuntos \n")
+            
+            print("Para assuntos relacionados à plataforma Brasil Participativo, por favor, entre em contato pelo e-mail: participacaodigital@presidencia.gov.br\nPara outros assuntos, registre a sua manifestação na Plataforma Integrada de Ouvidoria e Acesso à Informação (Fala.BR) https://falabr.cgu.gov.br\n")
+            
+            print("\nSua demanda foi atendida?\n")
+        
+            opcao2 = input("Digite 1 - Sim\n"+
+                        "Digite 2 - Não\n"+
+                        "Caso queira encerrar o atendimento, digite \"sair\"\n\n")
+            
+            match opcao2:
+                
+                case "1":
+                    print("\nAgradecemos o seu contato e continuamos à disposição!\n"+
+                            "Para acompanhar todas as atividades realizadas pelo Brasil Participativo, acesse https://brasilparticipativo.presidencia.gov.br \n")
+                    break;
+                
+                        
+                case "2": 
+                    opcao3 = input("\nDigite 9 - Retornar ao menu anterior;\n"+"Caso queira encerrar o atendimento, digite \"sair\".\n")
+                    
+                    match opcao3:
+                        case "9":
+                            continue;
+                        
+                        case "sair":
+                            break;
+                
+                case "sair":
+                    break;
 
-    case 3:
-        print("Você escolheu a opção 3 - Dificuldade de acesso")
-
-    case 4:
-        print("Você escolheu a opção 4 - Represento um órgão da Administração Pública Federal")
-    case 5:
-        print("Você escolheu a opção 5 - Sou da imprensa")
-    case 6:
-        print("Você escolheu a opção 6 - Perguntas Frequentes")
-    
-    case 7:
-        print("Você escolheu a opção 7 - Sair")
