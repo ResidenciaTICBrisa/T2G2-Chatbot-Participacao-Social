2a35b653b1cdca14916c57645f78809aefb37f09
Isaque <isak369@hotmail.com>
2024-03-13 13:00:09 +0300

    Implementando integração com Telegram #19
    Refatorando.

--- integram.py
+++ integram.py
@@ -18,37 +18,25 @@ def send_welcome(message):
     index_function = -1
 
 
-@bot.message_handler(func=lambda message: not message.text.isdigit() and index_function == -1)
+@bot.message_handler(func=lambda message: not message.text.isdigit() and index_function == -1 and message.text)
 def send_greeting(message):
     global index_function
     id_chat = message.chat.id
     bot.send_message(id_chat, f"Olá, {message.text}. Digite o número que corresponde à opção que deseja acessar. "
-                     f"Caso queira encerrar o atendimento, digite 'sair' a qualquer momento")
+                              f"Caso queira encerrar o atendimento, digite 'sair' a qualquer momento")
     bot.send_message(message.chat.id, "Digite 1 - O que é o Brasil Participativo?\n" +
-                                    "Digite 2 - Processos Participativos\n" +
-                                    "Digite 3 - Dificuldade de acesso\n" +
-                                    "Digite 4 - Represento um órgão da Administração Pública Federal\n" +
-                                    "Digite 5 - Sou da imprensa\n" +
-                                    "Digite 6 - Perguntas Frequentes\n" +
-                                    "Digite 7 - Outros Assuntos\n")
+                     "Digite 2 - Processos Participativos\n" +
+                     "Digite 3 - Dificuldade de acesso\n" +
+                     "Digite 4 - Represento um órgão da Administração Pública Federal\n" +
+                     "Digite 5 - Sou da imprensa\n" +
+                     "Digite 6 - Perguntas Frequentes\n" +
+                     "Digite 7 - Outros Assuntos\n")
     index_function = 0
 
 
+# primeiro bloco de perguntas
 @bot.message_handler(
     func=lambda message: message.text.isdigit() and (1 <= int(message.text) <= 7) and index_function == 0)
-=======
-    bot.reply_to(message,"Bem-vindo(a) ao atendimento virtual do Brasil Participativo. Para começar, diga-nos o seu nome")
-    bot.send_message(message.chat.id, "Digite 1 - O que é o Brasil Participativo?\n" + 
-                                    "Digite 2 - Processos Participativos\n" + 
-                                    "Digite 3 - Dificuldade de acesso\n" + 
-                                    "Digite 4 - Represento um órgão da Administração Pública Federal\n" + 
-                                    "Digite 5 - Sou da imprensa\n" + 
-                                    "Digite 6 - Perguntas Frequentes\n" + 
-                                    "Digite 7 - Outros Assuntos\n")
-
-#primeiro bloco de perguntas
-
-@bot.message_handler(func=lambda message: message.text.isdigit() and (1 <= int(message.text) <= 7) and index_function == 0)
 def handle_messages(message):
     global index_function
     id_chat = message.chat.id
@@ -65,43 +53,46 @@ def handle_messages(message):
             bot.send_message(id_chat, "Sua demanda foi atendida? \n1 - Sim \n2 - Não")
 
 
-#penultimo bloco de perguntas
-@bot.message_handler(func=lambda message: message.text.isdigit() and (1 <= int(message.text) <= 7) and index_function == 1)
+# penultimo bloco de perguntas
+@bot.message_handler(
+    func=lambda message: message.text.isdigit() and (1 <= int(message.text) <= 7) and index_function == 1)
 def handle_messages(message):
     global index_function
     id_chat = message.chat.id
     match message.text:
         case "1":
-            bot.send_message(id_chat,"Agradecemos o seu contato e continuamos à disposição! Para acompanhar todas as atividades realizadas pelo Brasil Participativo, acesse https://brasilparticipativo.presidencia.gov.br/")
+            bot.send_message(id_chat,
+                             "Agradecemos o seu contato e continuamos à disposição! Para acompanhar todas "
+                             "as atividades realizadas pelo Brasil Participativo, acesse "
+                             "https://brasilparticipativo.presidencia.gov.br/")
         case "2":
             index_function = 2
-            bot.send_message(id_chat, "Digite 9 - Retornar ao menu anterior ?\nCaso queira encerrar o atendimento, digite “sair”")
+            bot.send_message(id_chat,
+                             "Digite 9 - Retornar ao menu anterior ?\nCaso queira encerrar o atendimento, digite “sair”")
 
 
-
-
-#ultimo bloco de perguntas
-@bot.message_handler(func=lambda message: message.text == "9"  and index_function == 2)
+# ultimo bloco de perguntas
+@bot.message_handler(func=lambda message: message.text == "9" and index_function == 2)
 def handle_messages(message):
     global index_function
     id_chat = message.chat.id
     match message.text:
         case "9":
             bot.send_message(message.chat.id, "Digite 1 - O que é o Brasil Participativo?\n" +
-                                    "Digite 2 - Processos Participativos\n" +
-                                    "Digite 3 - Dificuldade de acesso\n" +
-                                    "Digite 4 - Represento um órgão da Administração Pública Federal\n" +
-                                    "Digite 5 - Sou da imprensa\n" +
-                                    "Digite 6 - Perguntas Frequentes\n" +
-                                    "Digite 7 - Outros Assuntos\n")
+                             "Digite 2 - Processos Participativos\n" +
+                             "Digite 3 - Dificuldade de acesso\n" +
+                             "Digite 4 - Represento um órgão da Administração Pública Federal\n" +
+                             "Digite 5 - Sou da imprensa\n" +
+                             "Digite 6 - Perguntas Frequentes\n" +
+                             "Digite 7 - Outros Assuntos\n")
             index_function = 0
 
 
 @bot.message_handler(func=lambda message: message.text == "sair")
 def handle_messages(message):
     id_chat = message.chat.id
-    bot.send_message(id_chat,"Saindo")
-    #precisamos ver como encerrar o atendimento
+    bot.send_message(id_chat, "Saindo")
+    # precisamos ver como encerrar o atendimento
 
 
 bot.infinity_polling()
